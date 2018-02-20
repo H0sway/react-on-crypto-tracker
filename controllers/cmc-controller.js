cfbc80b612799f01054d8dbfb2a9da0ab98b3f84
Joshua Russell <jkrussell756@gmail.com>
2018-02-20 13:34:47 -0500

    API call controller added

---
+++ controllers/cmc-controller.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const cmcController = {};
+
+controller.top = (req,res) => {
+  axios({
+    method: 'GET',
+    url: 'https://api.coinmarketcap.com/v1/ticker/?limit=50'
+  })
+  .then(cryptos => {
+    res.json({
+      message: 'Made an API call',
+      data: cryptos.data
+    });
+  })
+  .then(err => {
+    console.log('Top 50 CMC API call error', err);
+  });
+};
+
+controller.tracker = (req,res) => {
+  axios({
+    method: 'GET',
+    url: `https://api.coinmarketcap.com/v1/ticker/${currency.currency_id}`
+  })
+  .then(crypto => {
+    res.json({
+      message: 'Single crypto data loaded',
+      data: crypto.data
+    })
+  })
+  .catch(err => {
+    console.log('Single CMC call error', err);
+  });
+};
