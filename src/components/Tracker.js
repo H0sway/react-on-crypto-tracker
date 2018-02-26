a2c7d9c8e313c8c1ca1631481a1d366a7b998275
Joshua Russell <jkrussell756@gmail.com>
2018-02-26 10:25:11 -0500

    working on super error

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -5,41 +5,29 @@ import { LinkContainer } from 'react-router-bootstrap';
 import TrackerTable from './TrackerTable';
 
 class Tracker extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       dataLoaded: false,
       trackerData: [],
-      profile: {},
-    }
+    };
   }
   componentDidMount() {
-  //   axios({
-  //     method: 'POST',
-  //     url: `api/tracker/${user.id}`,
-  //   })
-  //   .then(currencies => {
-  //     this.setState({
-  //       dataLoaded: true,
-  //       trackerData: currencies.data.data,
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.log('api/tracker call error', err);
-  //   });
-    const { userProfile, getProfile } = this.props.auth;
-    if (!userProfile) {
-      getProfile((err, profile) => {
-        this.setState({ profile });
-      });
-    } else {
-      this.setState({ profile: userProfile });
-    }
-
-  }
-  login() {
-    this.props.auth.login();
-  }
+  console.log(this.props.profile);
+   axios({
+    method: 'POST',
+    url: `api/tracker/${this.props.profile.sub}`,
+   })
+   .then(currencies => {
+     this.setState({
+       dataLoaded: true,
+       trackerData: currencies.data.data,
+     });
+   })
+   .catch(err => {
+     console.log('api/tracker call error', err);
+   });
+  } 
   renderTable() {
     if (this.state.trackerData.length) {
      this.state.trackerdata.map(currency => {
@@ -58,34 +46,12 @@ class Tracker extends Component {
     }
   }
   render() {
-    console.log(this.state.profile);
-    const { isAuthenticated } = this.props.auth;
     return (
       <div className="Tracker">
-        {
-          isAuthenticated() && (
-              <div>
-                {this.state.dataLoaded ? this.renderTable() : <p>Loading... This could take a while</p>}
-              </div>
-            )
-        }
-        {
-          !isAuthenticated() && (
-              <h4>
-                You are not logged in! Please{' '}
-                <a
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                </a>
-                {' '}to continue.
-              </h4>
-            )
-        }
+        {this.state.dataLoaded ? this.renderTable() : <p>Loading... This could take a while</p>}
       </div>
     );
-  }
+ }
 }
 
 export default Tracker;
--- src/components/TrackerWrapper.js
+++ src/components/TrackerWrapper.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import TrackerWrapper from './TrackerWrappper';
+import Tracker from './Tracker';
 
 export default class TrackerWrapper extends React {
+  constructor() {
+    super();
+    this.state = {
+      profile: {},
+    }
+  }
+  componentDidMount() {
+    const { userProfile, getProfile } = this.props.auth;
+    if (!userProfile) {
+      getProfile((err, profile) => {
+        this.setState({ profile });
+      });
+    } else {
+      this.setState({ profile: userProfile });
+    }
+  }
+  login() {
+    this.props.auth.login();
+  }
   render() {
     return (
       <div className="TrackerWrapper">
+        {
+          isAuthenticated() && (
+              <div>
+                <Tracker profile={this.state.profile} />
+              </div>
+            )
+        }
+        {
+          !isAuthenticated() && (
+              <h4>
+                You are not logged in! Please{' '}
+                <a
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                </a>
+                {' '}to continue.
+              </h4>
+            )
+        }
+
       </div>
     );
   }
